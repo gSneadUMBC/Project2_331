1bfa0c5b3b72fc58227b221543bd89b43aedc56b
drollo1 <drollo1@umbc.edu>
2015-04-09 16:49:39 -0400

    Things changed are in the testing if's

--- studentScheduler.php
+++ studentScheduler.php
@@ -18,10 +18,32 @@ Group
 
 What day would you like to look at?
 <?php
-$currentMonth = $_SESSION["CurrMonth"];
-echo("The month is " .$_SESSION["CurrMonth"]);
-include("$currentMonth".".html");
+$testing = True;
 
+$months = array("January", "Febuary", "March", "April", "May" );
+
+if ($_SESSION["MonthInt"]==null && $testing){
+for ($i = 0; $months[$i] != "April"; $i++){
+	$_SESSION["MonthInt"] = $i; 
+}
+	echo("Initial set up");
+	$_SESSION["MonthInt"] ++;
+	$monthInt = $_SESSION["MonthInt"];
+}
+echo("<br>" . $_SESSION["MonthInt"] . "<br>");
+
+if ($testing)
+{
+	$currentMonth = $months[$_SESSION["MonthInt"]];
+	echo("<br>The month is " . $currentMonth);
+	include($currentMonth . ".html");
+
+}
+else{
+	$currentMonth = $_SESSION["CurrMonth"];
+	echo("<br>The month is " . $_SESSION["CurrMonth"]);
+	include($currentMonth . ".html");
+}
 ?>
 
 
@@ -35,6 +57,7 @@ include("$currentMonth".".html");
   	$COMMON = new common($debug);	
  	$date =$_POST['calDate'];
 	$type = $_POST['appointment'];
+	$monthChange = $_POST['prev'];
 
 	echo("Selected an avalable appointment from the list below or choose another day above.");
 	echo("<br>");
@@ -71,7 +94,6 @@ include("$currentMonth".".html");
 	    	echo("<tr>" . "<td>" ."<input type='radio' name='chosenAppt' value = $row[0] >"."</td>");
 		echo("<td>".$row[1]."</td>"."<td>".$row[3]."</td>"."<td>".$row[4]."</td>"."<td>".$row[5]."</td>"."<td>".$row[6]."</td>");	   
 
-
 		echo("</tr>");    
 	  }
 	$picked = $_POST['chosenAppt'];
@@ -81,16 +103,24 @@ include("$currentMonth".".html");
 	$sql = "select * from `Adv_made_Appts` WHERE `id` = '$picked'";
 	$rs = $COMMON->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
 	$row = mysql_fetch_row($rs);
+	
 if($picked)
 {
-$studID = $_SESSION["student"];
+
 $sql=
 "INSERT INTO `student Appts` (`Student ID`,`Date`, `Time`, `Advisor`, `Advisor E-mail`) 
-VALUES ('$studID','$row[2]','$row[1]','$row[5]','$row[6]')"; 
-$rs = $COMMON->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
-
-$sql = "UPDATE `Adv_made_Appts` SET `Slots`=`Slots` - 1 WHERE `date` = '$row[2]' AND `time` = '$row[1]' AND `Advisor`= '$row[5]'";
+VALUES ('UA54617','$row[2]','$row[1]','$row[5]','$row[6]')"; 
 $rs = $COMMON->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
 }
-$_SESSION["CurrMonth"] = $_POST['prev']
+
+if ($testing){
+echo("Before if " . $_SESSION["MonthInt"]);
+if ( $_POST['prev'] == "prev"){
+	$_SESSION["MonthInt"]--;
+	echo("After if " . $_SESSION["MonthInt"]);	
+	echo("<br>pressed Prev");
+}
+elseif( $_POST['next'] == next)
+	$_SESSION["MonthInt"]++;
+}
 ?>
