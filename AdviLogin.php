501adb0237c6e86572923dfd8e08324f8dc2d2ef
Galen Snead <galen690@gmail.com>
2015-04-19 14:13:07 -0400

    Added password verification for advisors

--- AdviLogin.php
+++ AdviLogin.php
@@ -27,15 +27,30 @@ $_SESSION["CurrMonth"] = $mydate[month];
 </form>
 <br>
 <form action='schedule.php' >
-<input  type="submit" value = "schedule appointments">
+<input  type="submit" value = "view & schedule appointments">
 </form>
-<form action='adviViewAppt.php' >
-<input  type="submit" value = "view appointments">
-</form>
+
 <?php
-$_SESSION["user"]= $_GET['email'];
+include("CommonMethods.php");
+$COMMON = new common($debug);
+$pwd = $_GET['pwd'];
+$_SESSION["user"] = $_GET['email'];
+
+$temp = $_GET['email'];
+$sql = "select `password` from `Advisors` where `E-mail` ='$temp' ";
+$rs = $COMMON->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
+$row = mysql_fetch_row($rs);
+
+if ($pwd == $row[0])
+{
+
 $user = $_SESSION["user"];
 echo ("Successfully logged in with: ".$user);
+}
+else
+{
+echo("Invalid Password. Please try again.");
+}
 ?>
 </body>
 </html>
