e6a9024641313281a92cf913355d96a8987a15db
drollo1 <drollo1@umbc.edu>
2015-04-20 13:27:22 -0400

    schedule update
    Displays appointment info on same page

--- schedule.php
+++ schedule.php
@@ -17,6 +17,7 @@ $COMMON = new common($debug);
 
 
 $AdEmail = $_SESSION["user"];
+$picked = $_GET['chosenAppt'];
 
 $sql= "SELECT `fname` FROM `Advisors` WHERE `E-mail` = '$AdEmail'";
 $rs1 = $COMMON->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
@@ -79,6 +80,7 @@ if ($_GET['calDate'] || $_GET['schedule'])
 	echo("<input type='text' name='grpSize' value='10'>");
 	echo("</td></tr></table>");
 
+//For scheduling appointments
 	echo("<input type='submit' name='schedule' value='Submit'>");
 	echo("</form>");
 	echo("<br>");
@@ -99,14 +101,18 @@ if ($_GET['calDate'] || $_GET['schedule'])
 			values('$apptTime','$date', '$advtype','$grpsize','$Advisor','$AdEmail')";
 			$rs = $COMMON->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
 		}
-		else 
-			echo("You did not enter a group size");
+		else{
+			$sql = "insert into `Adv_made_Appts`
+			(`time`,`date`,`type`,`Slots`,`Advisor`,`Advisor Email`)
+			values('$apptTime','$date', '$advtype','1','$Advisor','$AdEmail')";
+			$rs = $COMMON->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);		
+		}
 	}	
 echo("<h3>". $user. "'s schedule");
 	echo("<br>");
 
 //Scheduled appointment view 
-	echo("<form action='adviViewAppt.php' method='GET' name='form2'>");
+	echo("<form action='schedule.php' method='GET' name='form2'>");
 	$user = $_SESSION["user"];
 	$sql = "select * from `Adv_made_Appts` WHERE `date` = '$date' AND `Advisor Email` = '$AdEmail'";
 
@@ -123,21 +129,66 @@ echo("<h3>". $user. "'s schedule");
 
 	while($row = mysql_fetch_row($rs))
 	  {     
-
 		$stdDate = date("g:i a", strtotime("$row[1]"));
+
 	    	echo("<tr>" . "<td align='center'>" ."<input type='radio' name='chosenAppt' value = $row[0] >"."</td>");
 		echo("<td align='center'>".$row[3]."</td>"."<td align='right'>".$stdDate."</td>");	   
 
 		echo("</tr>");    
 	  }
 
-	$picked = $_GET['chosenAppt'];
-
 	echo("</table>");
-	echo("<input type='submit' value='View Appointment details'>");
+	echo("<input type='submit' name='details' value='View Appointment details'>");
 	echo("</form>");
 
 }
+if ($_GET['details'] && $picked){
+	$debug= false;
+	$COMMON = new common($debug);	
+
+	//picks time of selected appointment
+	$sql = "select * from `student Appts` WHERE `Appt_id` = '$picked'";
+	$rs = $COMMON->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
+	
+	$studIDs = array();
+	while($row = mysql_fetch_row($rs))
+	{
+		$inside = in_array($row[0],$studIDs);
+		if(!$inside){
+			$studIDs[] = $row[2];	
+			$detailDate = $row[3];
+			$detailTime = date("g:i a", strtotime("$row[4]"));
+		}
+	}
+	if($picked){
+	echo("<br>");
+	echo("<table border='1px'>");
+	echo("<th align='center' colspan = '5'> Displaying Students attending advising on ". $detailDate. " at ". $detailTime.  "</th>");
+       	echo("<tr>");
+        echo("<td align='center'>" . "<strong>Student ID" . "</td>");
+        echo("<td align='center'>" . "<strong>Major" . "</td>");
+	echo("<td align='center'>" . "<strong>First Name" . "</td>");
+ 	echo("<td align='center'>" . "<strong>Last Name" . "</td>");
+ 	echo("<td align='center'>" . "<strong>E-mail" . "</td>");
+	echo("</tr>");
+
+	for($i = 0;$i < count($studIDs);$i++)
+	{
+		$sql = "select * from `Students` WHERE `Student ID` = '$studIDs[$i]'";
+		$rs = $COMMON->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
+		$row = mysql_fetch_row($rs);
+
+		echo("<tr>");
+        	echo("<td>" . "$row[1]" . "</td>");
+        	echo("<td>" . "$row[2]" . "</td>");
+		echo("<td>" . "$row[3]" . "</td>");
+ 		echo("<td>" . "$row[4]" . "</td>");
+ 		echo("<td>" . "$row[5]" . "</td>");
+		echo("</tr>");
+	}
+}
+
+}	
 
 ?>
 
